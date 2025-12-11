f2eb9b1d0db44cde8bb27381d624fc91a5bd9a06
Kriti Singa <kritikumari841436@gmail.com>
2025-12-11 11:17:00 +0530

    nest version

--- backend/server.js
+++ backend/server.js
@@ -37,5 +37,15 @@ if (process.env.NODE_ENV === 'production') {
   });
 }
 
+if (process.env.NODE_ENV === 'production') {
+  const frontendPath = path.join(__dirname, "../frontend/build");
+
+  app.use(express.static(frontendPath));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(frontendPath, "index.html"));
+  });
+}
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, ()=> console.log(`Server running on ${PORT}`));
